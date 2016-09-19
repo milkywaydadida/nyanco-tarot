9664189e429fe5b024c0c956149f310abf8bf674
milkywaydadida <info@milky-d.com>
2016-09-19 17:14:58 +0900

    エラー対策

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tarot::Application.config.secret_token = '0921d42b6da5d39a33764236953aa9185db6b19417e2fa46fe0274c6b0982c4bbb5a923fd55c44ea6b2f22bce774455d75f758bb73940f7f73a7094869ca6186'
+
+Tarot::Application.config.secret_key_base = secure_token
