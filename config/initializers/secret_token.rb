350b85dce87e54b72f0e0f26cc01f44ee3cc7de1
milkywaydadida <info@milky-d.com>
2016-09-19 17:20:52 +0900

    やっぱりいらない

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,5 +5,3 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Tarot::Application.config.secret_token = '0921d42b6da5d39a33764236953aa9185db6b19417e2fa46fe0274c6b0982c4bbb5a923fd55c44ea6b2f22bce774455d75f758bb73940f7f73a7094869ca6186'
-
-Tarot::Application.config.secret_key_base = secure_token
